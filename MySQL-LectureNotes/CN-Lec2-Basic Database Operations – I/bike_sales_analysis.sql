5e94e1a8e8588d8f25906185981074825176a0d2
KrishnaMentor <krishnainstructor@gmail.com>
2025-05-22 23:55:03 +0530

    Add files via upload

---
+++ MySQL-LectureNotes/CN-Lec2-Basic Database Operations – I/bike_sales_analysis.sql
@@ -0,0 +1,9 @@
+SHOW DATABASES;
+CREATE DATABASE bike_sales;
+-- DROP DATABASE bike_sales;
+USE bike_sales;
+SELECT DATABASE();
+SHOW TABLES;
+DESC customers;
+SELECT * FROM Customers;
+-- DROP TABLE Customers;
